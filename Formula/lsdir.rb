0381bc54eac1f1605783aab3b2909f806e23da39
Noah Blair <noahjblair@outlook.com>
2024-11-21 09:11:53 -0700

    Bump version to 0.1.2

--- Formula/lsdir.rb
+++ Formula/lsdir.rb
@@ -3,7 +3,7 @@ class Lsdir < Formula
 
   desc "Enhanced directory listing tool with content preview"
   homepage "https://github.com/sodium-hydroxide/lsdir"
-  url "https://github.com/sodium-hydroxide/lsdir/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/sodium-hydroxide/lsdir/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
