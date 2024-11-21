5fcd14e6edd34f1df80bd81ccd574d22d647650a
Noah Blair <noahjblair@outlook.com>
2024-11-21 09:12:00 -0700

    Update SHA256 for v0.1.2

--- Formula/lsdir.rb
+++ Formula/lsdir.rb
@@ -4,14 +4,14 @@ class Lsdir < Formula
   desc "Enhanced directory listing tool with content preview"
   homepage "https://github.com/sodium-hydroxide/lsdir"
   url "https://github.com/sodium-hydroxide/lsdir/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "4dad148cbb19c6aad72773d1b4fbbfbdd3958b9ba10efda41aa26e99926f3d4c"
   license "MIT"
 
   depends_on "python@3.11"
 
   resource "python-magic" do
     url "https://files.pythonhosted.org/packages/da/db/0b3e28ac047452d079d375ec6798bf76a036a08182dbb39ed38116a49130/python-magic-0.4.27.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "4dad148cbb19c6aad72773d1b4fbbfbdd3958b9ba10efda41aa26e99926f3d4c"
   end
 
   def install
